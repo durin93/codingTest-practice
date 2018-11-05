77bc379863ff299b3ed38952fba2eed5c8b5df0a
durin93 <lsc401@naver.com>
2018-11-06 01:28:48 +0900

    선택정렬수정

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -1,31 +1,36 @@
 public class BubbleSort {
 
 
-    public static int[] bubbleSortAsc(int[] items) {
+    public static int[] bubbleSortAsc(int[] arr) {
 
-        for (int i = 0; i < items.length - 1; i++) {
-            for (int j = 0; j < items.length - 1; j++) {
-                if (items[j] > items[j + 1]) {
-                    int temp = items[j];
-                    items[j] = items[j + 1];
-                    items[j + 1] = temp;
+
+        for(int i = 0; i < arr.length-1; i++){
+
+            for(int j = 0; j < arr.length-1; j++){
+
+                if(arr[j] > arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
 
                 }
+
             }
 
         }
 
-        return items;
+        return arr;
     }
+
 
     public static int[] bubbleSortDesc(int[] items) {
 
-        for(int i = 0; i < items.length-1; i++){
-            for(int j = 0; j < items.length-1; j++){
-                if(items[j]<items[j+1]) {
+        for (int i = 0; i < items.length - 1; i++) {
+            for (int j = 0; j < items.length - 1; j++) {
+                if (items[j] < items[j + 1]) {
                     int temp = items[j];
-                    items[j] = items[j+1];
-                    items[j+1] = temp;
+                    items[j] = items[j + 1];
+                    items[j + 1] = temp;
                 }
             }
         }
--- src/SelectionSort.java
+++ src/SelectionSort.java
@@ -1,43 +1,45 @@
 public class SelectionSort {
 
-    public static int[] selectSortAsc(int [] items){
+    public static int[] selectSortAsc(int[] items) {
 
-        int indexMin;
 
-        for(int i=0; i < items.length-1; i++){
-            indexMin = i;
-            for(int j = i+1; j<items.length; j++){
-                if(items[indexMin] > items[j]){
-                    indexMin = j;
+        int min;
+
+        for (int i = 0; i < items.length - 1; i++) {
+            min = i;
+
+            for (int j = i + 1; j < items.length; j++) {
+                if (items[min] > items[j]) {
+                    min = j;
                 }
             }
 
-            int temp = items[indexMin];
-            items[indexMin] = items[i];
-            items[i]=temp;
-
-
+            if (min != i) {
+                int temp = items[min];
+                items[min] = items[i];
+                items[i] = temp;
+            }
         }
 
         return items;
     }
 
 
-    public static int[] selectSortDesc(int [] items){
+    public static int[] selectSortDesc(int[] items) {
 
         int indexMin;
 
-        for(int i=0; i < items.length-1; i++){
+        for (int i = 0; i < items.length - 1; i++) {
             indexMin = i;
-            for(int j = i+1; j<items.length; j++){
-                if(items[indexMin] < items[j]){
+            for (int j = i + 1; j < items.length; j++) {
+                if (items[indexMin] < items[j]) {
                     indexMin = j;
                 }
             }
 
             int temp = items[indexMin];
             items[indexMin] = items[i];
-            items[i]=temp;
+            items[i] = temp;
 
 
         }
